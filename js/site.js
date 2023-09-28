62aecc88ee4171c5750b6ccdd0fe1a956b4657c2
Bryan <127161500+TheWebCoder@users.noreply.github.com>
2023-09-27 21:44:29 -0700

    Added modal, new event, table, and fixed bugs
    Added a modal with the ability to add a new event as well as a table displaying all of the stats. Lastly the code was cleaned and some bugs were fixed.

--- js/site.js
+++ js/site.js
@@ -64,68 +64,148 @@
     },
 ];
   
-//the default display for all events
-let filteredEvents = events;
-
 //build a dropdown of specific cities
 function buildDropDown() {
+
+    //grab the select list we want to add city names to.
     let eventDD = document.getElementById("eventDropDown");
-    //distinct events for the events array
-    let distinctEvents = [...new Set(events.map(event => event.city))];
-    
-    let linkHTMLEnd = '<div class="dropdown-divider"></div><a class="dropdown-item" onclick="getEvents(this)" data-string="All">All</a>';
-
-    let resultHTML = "";
-
-    for (let i = 0; i < distinctEvents.length; i++) {
-        resultHTML += `<a class="dropdown-item" onclick="getEvents(this)" data-string="${distinctEvents[i]}">${distinctEvents[i]}</a>`;
+    eventDD.innerHTML = "";
+    //grab a template we want to use to populate the select list
+    const template = document.getElementById("cityDD-template");
+  
+    //Pull the events from local storage if there are none pull form the
+    //default data.
+    curEvents = JSON.parse(localStorage.getItem("eventsArray"));
+    if (curEvents == null) {
+      curEvents = events;
     }
-
-    resultHTML += linkHTMLEnd;
-    eventDD.innerHTML = resultHTML;
-}
-
-function getEvents(element) {
+  
+    let distinctEvents = [...new Set(curEvents.map((event) => event.city))];
+    let ddItemNode = document.importNode(template.content, true);
+    ddItem = ddItemNode.querySelector("a");
+    ddItem.setAttribute("data-string", "All");
+    ddItem.textContent = "All";
+    eventDD.appendChild(ddItem);
+  
+    for (var i = 0; i < distinctEvents.length; i++) {
+      ddItemNode = document.importNode(template.content, true);
+      ddItem = ddItemNode.querySelector("a");
+      ddItem.setAttribute("data-string", distinctEvents[i]);
+      ddItem.textContent = distinctEvents[i];
+      eventDD.appendChild(ddItem);
+    }
+    displayStats(curEvents);
+    displayData();
+  }
+  
+  //show stats for a specific location
+  function getEvents(element) {
     let city = element.getAttribute("data-string");
-    filteredEvents = events;
-    document.getElementById("statsHeader").innerHTML = `Stats for ${city} Events`;
-    if ( city != "All") {
-        filteredEvents = events.filter(function (item) {
-            if(item.city == city) {
-                return item;
-            }
-        })
+    curEvents = JSON.parse(localStorage.getItem("eventsArray")) || events;
+    let filteredEvents = curEvents;
+  
+    document.getElementById("statsHeader").innerHTML = `Stats For ${city} Events`;
+    if (city != "All") {
+      //Explain how array filtering works-
+      filteredEvents = curEvents.filter(function (item) {
+        if (item.city == city) {
+          return item;
+        }
+      });
     }
-
-    displayStats();
-
-}
-
-//display the stats
-function displayStats() {
+    displayStats(filteredEvents);
+  }
+  
+  function displayStats(filteredEvents) {
     let total = 0;
     let average = 0;
     let most = 0;
     let least = -1;
     let currentAttendance = 0;
-
-    for (var i = 0; i < filteredEvents.length; i++) {
-        currentAttendance = filteredEvents[i].attendance;
-        total += currentAttendance;
-
-        if (most < currentAttendance) {
-            most = currentAttendance;
-        }
-
-        if ( least > currentAttendance || least < 0) {
-            least = currentAttendance;
-        }
+  
+    //display total attendance
+    for (let i = 0; i < filteredEvents.length; i++) {
+      currentAttendance = filteredEvents[i].attendance;
+      total += currentAttendance;
+  
+      if (most < currentAttendance) {
+        most = currentAttendance;
+      }
+  
+      if (least > currentAttendance || least < 0) {
+        least = currentAttendance;
+      }
     }
-
     average = total / filteredEvents.length;
-
+  
     document.getElementById("total").innerHTML = total.toLocaleString();
     document.getElementById("most").innerHTML = most.toLocaleString();
     document.getElementById("least").innerHTML = least.toLocaleString();
-    document.getElementById("average").innerHTML = average.toLocaleString((undefined, {minimumFractionDigits: 0, maximumFractionDigits: 0}));
-}
+    document.getElementById("average").innerHTML = average.toLocaleString(
+      undefined, {
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0,
+      }
+    );
+  }
+  
+  function displayData() {
+    const template = document.getElementById("eventData-template");
+    const eventBody = document.getElementById("eventBody");
+    //clear table first
+    eventBody.innerHTML = "";
+    //grab the events from local storage
+    let curEvents = JSON.parse(localStorage.getItem("eventsArray")) || [];
+  
+    if (curEvents.length == 0) {
+      curEvents = events;
+      localStorage.setItem("eventsArray", JSON.stringify(curEvents));
+    }
+  
+    for (let i = 0; i < curEvents.length; i++) {
+      const eventRow = document.importNode(template.content, true);
+      //grab only the columns in the template
+      eventCols = eventRow.querySelectorAll("td");
+  
+      eventCols[0].textContent = curEvents[i].event;
+      eventCols[1].textContent = curEvents[i].city;
+      eventCols[2].textContent = curEvents[i].state;
+      eventCols[3].textContent = curEvents[i].attendance;
+      eventCols[4].textContent = new Date(
+        curEvents[i].date
+      ).toLocaleDateString();
+  
+      eventBody.appendChild(eventRow);
+    }
+  }
+  
+  function saveEventData() {
+    //grab the events out of local storage
+    let curEvents = JSON.parse(localStorage.getItem("eventsArray")) || events;
+  
+    //document.getElementById("newEventName");
+    let obj = {};
+    obj["event"] = document.getElementById("newEventName").value;
+    obj["city"] = document.getElementById("newEventCity").value;
+  
+    let stateSel = document.getElementById("newEventState");
+    obj["state"] = stateSel.options[stateSel.selectedIndex].text
+  
+    obj["attendance"] = parseInt(
+      document.getElementById("newEventAttendance").value,
+      10
+    );
+  
+    let eventDate = document.getElementById("newEventDate").value;
+    let eventDate2 = `${eventDate} 00:00`
+  
+    obj["date"] = new Date(eventDate2).toLocaleDateString();
+  
+    curEvents.push(obj);
+  
+    localStorage.setItem("eventsArray", JSON.stringify(curEvents));
+    //clear the form
+    //Access the values from the form by ID and add an object to the array.
+    buildDropDown();
+    displayData();
+  }
